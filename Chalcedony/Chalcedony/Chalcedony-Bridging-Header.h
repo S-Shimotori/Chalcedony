78f17f52b006c01badc0e5a77a508a035ba45d9f
Shigure Shimotori <S-Shimotori@S-Shimotoris-MacBook-Pro.local>
2015-06-19 21:21:41 +0900

    close #2 set bridging header file

---
+++ Chalcedony/Chalcedony/Chalcedony-Bridging-Header.h
@@ -0,0 +1,10 @@
+//
+//  Chalcedony-Bridging-Header.h
+//  Chalcedony
+//
+//  Created by S-Shimotori on 6/19/15.
+//  Copyright (c) 2015 S-Shimotori. All rights reserved.
+//
+
+#import "IQKeyboardManager/IQKeyboardManager.h"
+#import "PNChart/PNChart.h"
