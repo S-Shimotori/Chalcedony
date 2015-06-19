1bb8b58a0a5d5b85999d2e9e30b89a36fee99078
Shigure Shimotori <S-Shimotori@S-Shimotoris-MacBook-Pro.local>
2015-06-19 21:14:23 +0900

    add frameworks by cocoapods with #2

---
+++ Chalcedony/Pods/Target Support Files/Pods/Pods-environment.h
@@ -0,0 +1,98 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// IQKeyboardManager
+#define COCOAPODS_POD_AVAILABLE_IQKeyboardManager
+#define COCOAPODS_VERSION_MAJOR_IQKeyboardManager 3
+#define COCOAPODS_VERSION_MINOR_IQKeyboardManager 2
+#define COCOAPODS_VERSION_PATCH_IQKeyboardManager 4
+
+// OMGHTTPURLRQ
+#define COCOAPODS_POD_AVAILABLE_OMGHTTPURLRQ
+#define COCOAPODS_VERSION_MAJOR_OMGHTTPURLRQ 2
+#define COCOAPODS_VERSION_MINOR_OMGHTTPURLRQ 1
+#define COCOAPODS_VERSION_PATCH_OMGHTTPURLRQ 0
+
+// OMGHTTPURLRQ/FormURLEncode
+#define COCOAPODS_POD_AVAILABLE_OMGHTTPURLRQ_FormURLEncode
+#define COCOAPODS_VERSION_MAJOR_OMGHTTPURLRQ_FormURLEncode 2
+#define COCOAPODS_VERSION_MINOR_OMGHTTPURLRQ_FormURLEncode 1
+#define COCOAPODS_VERSION_PATCH_OMGHTTPURLRQ_FormURLEncode 0
+
+// OMGHTTPURLRQ/RQ
+#define COCOAPODS_POD_AVAILABLE_OMGHTTPURLRQ_RQ
+#define COCOAPODS_VERSION_MAJOR_OMGHTTPURLRQ_RQ 2
+#define COCOAPODS_VERSION_MINOR_OMGHTTPURLRQ_RQ 1
+#define COCOAPODS_VERSION_PATCH_OMGHTTPURLRQ_RQ 0
+
+// OMGHTTPURLRQ/UserAgent
+#define COCOAPODS_POD_AVAILABLE_OMGHTTPURLRQ_UserAgent
+#define COCOAPODS_VERSION_MAJOR_OMGHTTPURLRQ_UserAgent 2
+#define COCOAPODS_VERSION_MINOR_OMGHTTPURLRQ_UserAgent 1
+#define COCOAPODS_VERSION_PATCH_OMGHTTPURLRQ_UserAgent 0
+
+// PNChart
+#define COCOAPODS_POD_AVAILABLE_PNChart
+#define COCOAPODS_VERSION_MAJOR_PNChart 0
+#define COCOAPODS_VERSION_MINOR_PNChart 8
+#define COCOAPODS_VERSION_PATCH_PNChart 3
+
+// PromiseKit
+#define COCOAPODS_POD_AVAILABLE_PromiseKit
+#define COCOAPODS_VERSION_MAJOR_PromiseKit 2
+#define COCOAPODS_VERSION_MINOR_PromiseKit 0
+#define COCOAPODS_VERSION_PATCH_PromiseKit 4
+
+// PromiseKit/CorePromise
+#define COCOAPODS_POD_AVAILABLE_PromiseKit_CorePromise
+#define COCOAPODS_VERSION_MAJOR_PromiseKit_CorePromise 2
+#define COCOAPODS_VERSION_MINOR_PromiseKit_CorePromise 0
+#define COCOAPODS_VERSION_PATCH_PromiseKit_CorePromise 4
+
+// PromiseKit/Foundation
+#define COCOAPODS_POD_AVAILABLE_PromiseKit_Foundation
+#define COCOAPODS_VERSION_MAJOR_PromiseKit_Foundation 2
+#define COCOAPODS_VERSION_MINOR_PromiseKit_Foundation 0
+#define COCOAPODS_VERSION_PATCH_PromiseKit_Foundation 4
+
+// PromiseKit/QuartzCore
+#define COCOAPODS_POD_AVAILABLE_PromiseKit_QuartzCore
+#define COCOAPODS_VERSION_MAJOR_PromiseKit_QuartzCore 2
+#define COCOAPODS_VERSION_MINOR_PromiseKit_QuartzCore 0
+#define COCOAPODS_VERSION_PATCH_PromiseKit_QuartzCore 4
+
+// PromiseKit/UIKit
+#define COCOAPODS_POD_AVAILABLE_PromiseKit_UIKit
+#define COCOAPODS_VERSION_MAJOR_PromiseKit_UIKit 2
+#define COCOAPODS_VERSION_MINOR_PromiseKit_UIKit 0
+#define COCOAPODS_VERSION_PATCH_PromiseKit_UIKit 4
+
+// Realm
+#define COCOAPODS_POD_AVAILABLE_Realm
+#define COCOAPODS_VERSION_MAJOR_Realm 0
+#define COCOAPODS_VERSION_MINOR_Realm 93
+#define COCOAPODS_VERSION_PATCH_Realm 2
+
+// Realm/Headers
+#define COCOAPODS_POD_AVAILABLE_Realm_Headers
+#define COCOAPODS_VERSION_MAJOR_Realm_Headers 0
+#define COCOAPODS_VERSION_MINOR_Realm_Headers 93
+#define COCOAPODS_VERSION_PATCH_Realm_Headers 2
+
+// RealmSwift
+#define COCOAPODS_POD_AVAILABLE_RealmSwift
+#define COCOAPODS_VERSION_MAJOR_RealmSwift 0
+#define COCOAPODS_VERSION_MINOR_RealmSwift 93
+#define COCOAPODS_VERSION_PATCH_RealmSwift 2
+
+// UICountingLabel
+#define COCOAPODS_POD_AVAILABLE_UICountingLabel
+#define COCOAPODS_VERSION_MAJOR_UICountingLabel 1
+#define COCOAPODS_VERSION_MINOR_UICountingLabel 2
+#define COCOAPODS_VERSION_PATCH_UICountingLabel 0
+
