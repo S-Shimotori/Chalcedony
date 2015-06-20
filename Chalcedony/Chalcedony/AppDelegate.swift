7eded39e3521781e6db9ca6fcb7b519a545b7637
Shigure Shimotori <komata.h.aa@m.titech.ac.jp>
2015-06-20 15:23:42 +0900

    add init function

--- Chalcedony/Chalcedony/AppDelegate.swift
+++ Chalcedony/Chalcedony/AppDelegate.swift
@@ -17,8 +17,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Override point for customization after application launch.
         Fabric.with([Crashlytics()])
+
+        if !CCDSetting.sharedInstance().initialized {
+            CCDSetting.sharedInstance().initialize()
+        }
         return true
     }
 
