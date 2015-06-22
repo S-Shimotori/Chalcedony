71f5aff7ece7edc36704889a96e613bb2a72d404
Shigure Shimotori <komata.h.aa@m.titech.ac.jp>
2015-06-23 01:43:35 +0900

    add function to tweet

--- Chalcedony/Chalcedony/CCDTwitterModel.swift
+++ Chalcedony/Chalcedony/CCDTwitterModel.swift
@@ -12,7 +12,6 @@ class CCDTwitterModel {
             }
         }
     }
-    let apiClient = Twitter.sharedInstance().APIClient
     func login() {
         Twitter.sharedInstance().logInWithCompletion {
             (session, error) -> Void in
@@ -25,5 +24,32 @@ class CCDTwitterModel {
             }
         }
     }
+
+    private let apiClient = Twitter.sharedInstance().APIClient
+    private let POST = "POST"
+    private let updateJsonURL = "https://api.twitter.com/1.1/statuses/update.json"
+    private func getStatusParameter(message: String) -> [NSObject: AnyObject] {
+        return ["status": message]
+    }
+    func tweet(message: String, activityIndicatorView: UIActivityIndicatorView, completionOnSuccess: (() -> ())?, completionOnFailure: ((String) -> ())?) {
+        let request = apiClient.URLRequestWithMethod(POST, URL: updateJsonURL, parameters: getStatusParameter(message), error: nil)
+        apiClient.sendTwitterRequest(request){(response, data, error) in
+            if activityIndicatorView.isAnimating() {
+                activityIndicatorView.stopAnimating()
+            }
+            switch (response, data, error) {
+            case (.Some, .Some, .None):
+                if let completionOnSuccess = completionOnSuccess {
+                    completionOnSuccess()
+                }
+            default:
+                if let completionOnFailure = completionOnFailure {
+                    completionOnFailure(error!.localizedDescription)
+                }
+                println("\(response) \(data) \(error)")
+            }
+        }
+    }
+
 }
 
