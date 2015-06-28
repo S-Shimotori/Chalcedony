c72103e5485864a73adbce6adc4b8a641050e3b1
Shigure Shimotori <komata.h.aa@m.titech.ac.jp>
2015-06-28 12:54:42 +0900

    add function to compare two NSDates' date

---
+++ Chalcedony/Chalcedony/NSDate+extension.swift
@@ -0,0 +1,30 @@
+//
+//  NSDate+extension.swift
+//  Chalcedony
+//
+//  Created by S-Shimotori on 6/28/15.
+//  Copyright (c) 2015 S-Shimotori. All rights reserved.
+//
+
+import UIKit
+
+extension NSDate {
+    func date() -> (year: Int, month: Int, day: Int, weekday: Int) {
+        let calendar = NSCalendar.currentCalendar()
+        let components = calendar.components(.CalendarUnitYear | .CalendarUnitMonth | .CalendarUnitDay | .CalendarUnitWeekday, fromDate: self)
+        return (components.year, components.month, components.day, components.weekday)
+    }
+    func time() -> (hour: Int, minute: Int, second: Int) {
+        let calendar = NSCalendar.currentCalendar()
+        let components = calendar.components(.CalendarUnitHour | .CalendarUnitMinute | .CalendarUnitSecond, fromDate: self)
+        return (components.hour , components.minute, components.second)
+    }
+    func isEqualWithoutTime(otherNSDate: NSDate) -> Bool {
+        let selfDate = self.date()
+        let otherDate = otherNSDate.date()
+
+        return selfDate.year == otherDate.year
+            && selfDate.month == otherDate.month
+            && selfDate.day == otherDate.day
+    }
+}
