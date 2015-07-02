38c457fd3b50a91f65f5e9ef3663159e09d5803d
Shigure Shimotori <komata.h.aa@m.titech.ac.jp>
2015-07-02 18:02:11 +0900

    add copy right

--- Chalcedony/Chalcedony/CCDMessage.swift
+++ Chalcedony/Chalcedony/CCDMessage.swift
@@ -1,3 +1,10 @@
+//
+//  CCDMessage.swift
+//  Chalcedony
+//
+//  Created by S-Shimotori on 7/2/15.
+//  Copyright (c) 2015 S-Shimotori. All rights reserved.
+//
 
 struct CCDMessage {
     enum Status: String {
