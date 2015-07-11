f88e62917d1d50e853b7ae2f6109e7f7c3f8d17c
Shigure Shimotori <komata.h.aa@m.titech.ac.jp>
2015-07-11 14:54:48 +0900

    add Quick and Nimble with pod

---
+++ Chalcedony/Pods/Target Support Files/Pods-ChalcedonyTests-Realm/Pods-ChalcedonyTests-Realm-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ChalcedonyTests_Realm : NSObject
+@end
+@implementation PodsDummy_Pods_ChalcedonyTests_Realm
+@end
---
+++ Chalcedony/Pods/Target Support Files/Pods-ChalcedonyTests-RealmSwift/Pods-ChalcedonyTests-RealmSwift-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ChalcedonyTests_RealmSwift : NSObject
+@end
+@implementation PodsDummy_Pods_ChalcedonyTests_RealmSwift
+@end
