37c86447fc7825cddcdf878153b2490fab087755
Shigure Shimotori <komata.h.aa@m.titech.ac.jp>
2015-07-15 18:31:22 +0900

    add search function
    show map not yet

--- Chalcedony/Chalcedony/CCDLaboLocateViewController.swift
+++ Chalcedony/Chalcedony/CCDLaboLocateViewController.swift
@@ -9,11 +9,13 @@
 import UIKit
 import MapKit
 
-class CCDLaboLocateViewController: UIViewController, MKMapViewDelegate {
+class CCDLaboLocateViewController: UIViewController, MKMapViewDelegate, UISearchBarDelegate {
 
     private let initialLocation = (35.658774, 139.701361)
+    private var mapItem = [MKMapItem]()
 
     @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var searchBar: UISearchBar!
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -27,22 +29,55 @@ class CCDLaboLocateViewController: UIViewController, MKMapViewDelegate {
         region.span.longitudeDelta = 0.02
         mapView.setRegion(region,animated:true)
         mapView.mapType = .Standard
+
+        searchBar.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
 
-    /*
-    // MARK: - Navigation
+    func searchBarSearchButtonClicked(searchBar: UISearchBar) {
+        searchBar.resignFirstResponder()
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        let request = MKLocalSearchRequest()
+        request.naturalLanguageQuery = searchBar.text
+        request.region = mapView.region
+        let search = MKLocalSearch(request: request)
+
+        search.startWithCompletionHandler(){response, error in
+            /*
+            self.mapItem.removeAll(keepCapacity: false)
+            self.mapView.removeAnnotations(self.mapView.annotations)
+
+            for item in response.mapItems {
+                if let item = item as? MKMapItem {
+                    let point = MKPointAnnotation()
+                    point.coordinate = item.placemark.coordinate
+                    point.title = item.placemark.name
+                    point.subtitle = item.placemark.title
+                    self.mapView.addAnnotation(point)
+                    self.mapItem.append(item)
+                }
+            }
+
+            self.mapView.showAnnotations(self.mapView.annotations, animated: true)*/
+        }
+
     }
-    */
 
+    func searchBarCancelButtonClicked(searchBar: UISearchBar) {
+        searchBar.resignFirstResponder()
+    }
+
+    func searchBarShouldBeginEditing(searchBar: UISearchBar) -> Bool {
+        searchBar.setShowsCancelButton(true, animated: true)
+        return true
+    }
+
+    func searchBarShouldEndEditing(searchBar: UISearchBar) -> Bool {
+        searchBar.setShowsCancelButton(false, animated: true)
+        return true
+    }
 }
